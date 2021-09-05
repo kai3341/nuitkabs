1cd1057375aa4fa328e16adfbaff9ebfbf1b56e8
Anton Kovalevich <kai3341@gmail.com>
2021-09-05 14:24:36 +0300

    Fix license classifier

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ classifiers = [
     'Topic :: Software Development :: Build Tools',
 
     # Pick your license as you wish (should match "license" above)
-    'License :: OSI Approved :: GNU GPL v3',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
